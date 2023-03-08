1da3139d7c2545b3e02da36ef920e49c784c6797
3nnui <lex@immerda.ch>
2023-03-08 20:57:00 +0100

    refactor to main.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,34 @@
-use brim::cli::{Cli, Parser, Commands};
+use clap::{Parser, Subcommand};
+
+
+#[derive(Parser)]
+#[clap(arg_required_else_help = true)]
+#[command(name = "Brim")]
+#[command(author = "3nnui")]
+#[command(version = "1.0")]
+#[command(about = "
+▀█████████▄     ▄████████  ▄█    ▄▄▄▄███▄▄▄▄  
+  ███    ███   ███    ███ ███  ▄██▀▀▀███▀▀▀██▄
+  ███    ███   ███    ███ ███▌ ███   ███   ███
+ ▄███▄▄▄██▀   ▄███▄▄▄▄██▀ ███▌ ███   ███   ███
+▀▀███▀▀▀██▄  ▀▀███▀▀▀▀▀   ███▌ ███   ███   ███
+  ███    ██▄ ▀███████████ ███  ███   ███   ███
+  ███    ███   ███    ███ ███  ███   ███   ███
+▄█████████▀    ███    ███ █▀    ▀█   ███   █▀ 
+               ███    ███                     
+               
+Fedora post install tool for some moderate hardening...")]
+
+struct Cli {
+    #[command(subcommand)]
+    pub command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// does testing things
+    Test,
+}
 
 fn main() {
     let cli = Cli::parse();
