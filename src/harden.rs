fc313ed119cff623917ee9cb7066996c1c71557b
3nnui <lex@immerda.ch>
2023-03-09 11:52:40 +0100

    inquire config

--- src/harden.rs
+++ src/harden.rs
@@ -1,5 +1,25 @@
+use std::error::Error;
+use std::process;
+use inquire::{MultiSelect, InquireError};
+
 pub mod kernel;
 
-pub fn configure_harden() {
-    println!("init hardening");
+const featured_components: Vec<&str> = vec!["Kernel"];
+
+struct Config<'a> {
+    components: Vec<& 'a str>,
+}
+
+impl<'a> Config<'a> {
+    pub fn inquire() -> Result<Config<'a>, Box<dyn Error>> {
+        let ans = MultiSelect::new(
+            "Select the os Components which are to be hardened:",
+            featured_components)
+            .prompt().unwrap_or_else(|err: InquireError| {
+                println!("Problem parsing arguments: {}", err);
+                process::exit(1);
+            });
+
+       Ok(Config{ components: ans})
+    }
 }
