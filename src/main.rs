5bd1f5acb8c939444145eb7ad94c1924cf2aab08
3nnui <lex@immerda.ch>
2023-03-09 15:58:34 +0100

    display banner on subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
+use std::process;
 use clap::{Parser, Subcommand};
 
 mod harden;
 
-#[derive(Parser)]
-#[clap(arg_required_else_help = true)]
-#[command(name = "Brim")]
-#[command(author = "3nnui")]
-#[command(version = "1.0")]
-#[command(about = "
+const BANNER: &str = "
 ▀█████████▄     ▄████████  ▄█    ▄▄▄▄███▄▄▄▄  
   ███    ███   ███    ███ ███  ▄██▀▀▀███▀▀▀██▄
   ███    ███   ███    ███ ███▌ ███   ███   ███
@@ -18,7 +14,14 @@ mod harden;
 ▄█████████▀    ███    ███ █▀    ▀█   ███   █▀ 
                ███    ███                     
                
-Fedora post install tool for some moderate hardening...")]
+A fedora post install tool for some moderate hardening...";
+
+#[derive(Parser)]
+#[clap(arg_required_else_help = true)]
+#[command(name = "Brim")]
+#[command(author = "3nnui")]
+#[command(version = "1.0")]
+#[command(about = BANNER)]
 
 struct Cli {
     #[command(subcommand)]
@@ -26,6 +29,7 @@ struct Cli {
 }
 
 #[derive(Subcommand)]
+
 pub enum Commands {
     /// guided step by step os hardening
     Harden,
@@ -33,6 +37,8 @@ pub enum Commands {
 
 fn main() {
     let cli = Cli::parse();
+
+    println!("{}", BANNER);
 
     match &cli.command {
         Some(Commands::Harden) => harden::harden_os(),
